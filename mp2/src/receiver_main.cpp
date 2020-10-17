66b01b8e53972ec5cd5c84ff305c8019efcb48a3
Jun Luo <junluo2@illinois.edu>
2020-10-17 15:13:32 -0400

    Update receiver

--- mp2/src/receiver_main.cpp
+++ mp2/src/receiver_main.cpp
@@ -16,9 +16,10 @@
 #include <unistd.h>
 #include <pthread.h>
 
-#define RECV_BUF_SIZE 8192
+#include <unordered_map>
+
+#define RECV_BUF_SIZE 4096
 #define TCP_PACKET_SIZE 4096
-#define RING_BUF_SIZE 20
 #define BEGIN_SEQ_NUM 0
 
 #define DEBUG 0
@@ -42,30 +43,20 @@ void diep(const char *s) {
  * into the dest_file location and erases ring buffer location after each write.
  * Returns ACK sequence number to send back
  */
-unsigned int consecutiveWriteToFile(TCP_packet* ring_buf[], int LCP_ind, FILE* dest_file) {
+unsigned int writeToFile(unsigned data_size, char data[], FILE* dest_file) {
     size_t bytes_written = 0;
-    unsigned int send_back_ack_seq_no;
-    while (ring_buf[LCP_ind] != NULL) {
-        TCP_packet* packet = ring_buf[LCP_ind];
-        send_back_ack_seq_no = packet->seq_no;
-        bytes_written = fwrite(packet->data, 1, packet->data_size, dest_file);
-        // memcpy(&ring_buf[LCP_ind], 0, TCP_PACKET_SIZE);
-        ring_buf[LCP_ind] = NULL;
-        LCP_ind++;
-        printf("%ld bytes written\n", bytes_written);
-    }
-    return send_back_ack_seq_no;
+    bytes_written = fwrite(data, 1, data_size, dest_file);
+    return bytes_written;
 }
 
 void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
     int recv_bytes;
     char buf[RECV_BUF_SIZE];
     char ack_buf[100];
-    TCP_packet* ring_buf[RING_BUF_SIZE];
+    std::unordered_map<unsigned int, TCP_packet*> buffer;
 
     struct sockaddr_storage other_addr;
     socklen_t other_addr_len;
-    memset(ring_buf, 0, RING_BUF_SIZE);
 
     slen = sizeof (si_other);
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
@@ -82,18 +73,18 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
         diep("bind");
     }
 
-
-    other_addr_len = sizeof other_addr;
-    TCP_packet incoming_packet;
     FILE* dest_file;
     if ((dest_file = fopen(destinationFile, "wb")) == NULL) {
         diep("fopen");
     }
 
+    other_addr_len = sizeof other_addr;
+    TCP_packet* incoming_packet;
     bool last_packet_found = false;
-    unsigned int last_packet_seq_no;
+    unsigned int last_packet_seq_no = 0;
     // index of last consecutive packet in ring buffer
-    int LCP_ind = 0;
+    int nextPacketId = 0;
+    int send_back_ack_seq_no = nextPacketId - 1;
     while (true) {
         // receive data
         if ((recv_bytes = recvfrom(s, buf, RECV_BUF_SIZE, 0,
@@ -102,32 +93,39 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
             exit(1);
         }
         // decode and store data
-        memcpy(&incoming_packet, buf, TCP_PACKET_SIZE);
-        #ifdef DEBUG
-        printf("receive packet %d, content_size: %d\n",
-                incoming_packet.seq_no, incoming_packet.data_size);
-        #endif
-        ring_buf[incoming_packet.seq_no % RING_BUF_SIZE] = &incoming_packet;
+		incoming_packet = (TCP_packet*) malloc(TCP_PACKET_SIZE);
+		memcpy(incoming_packet, buf, TCP_PACKET_SIZE);
 
-        // check for last packet
-        if (incoming_packet.data_size == 0) {
+        if (incoming_packet->data_size == 0) {
             last_packet_found = true;
-            last_packet_seq_no = incoming_packet.seq_no;
+            last_packet_seq_no = incoming_packet->seq_no;
         }
+
         // write data to file
-        unsigned int send_back_ack_seq_no;
-        if (incoming_packet.seq_no == BEGIN_SEQ_NUM) { // write beginning packet, do not update LCP
-            // write all consecutive in ring buffer to file and update sendBackAck
-            send_back_ack_seq_no = consecutiveWriteToFile(ring_buf, LCP_ind, dest_file);
-            // update LCP index
-            LCP_ind = send_back_ack_seq_no % RING_BUF_SIZE;
-        } else if (incoming_packet.seq_no != BEGIN_SEQ_NUM &&
-                    incoming_packet.seq_no == LCP_ind + 1) { // write incoming packet if it is consecutive
-            LCP_ind++;
-            // write all consecutive in ring buffer to file and update sendBackAck
-            send_back_ack_seq_no = consecutiveWriteToFile(ring_buf, LCP_ind, dest_file);
-            // update LCP index
-            LCP_ind = send_back_ack_seq_no % RING_BUF_SIZE;
+        if (incoming_packet->seq_no == nextPacketId) {
+            // write to file
+            int bytes_written = writeToFile(incoming_packet->data_size, incoming_packet->data, dest_file);
+            printf("%d bytes written to file\n", bytes_written);
+            free(incoming_packet);
+            incoming_packet = NULL;
+            nextPacketId++;
+            // use a while loop to check cached packet in map and write them to file
+            while (buffer.find(nextPacketId) != buffer.end()) {
+                TCP_packet* packet = buffer.at(nextPacketId);
+                bytes_written = writeToFile(packet->data_size, packet->data, dest_file);
+                printf("%d bytes written to file\n", bytes_written);
+                buffer.erase(nextPacketId);
+                free(packet);
+                nextPacketId++;
+            }
+            send_back_ack_seq_no = nextPacketId - 1;
+        } else if (buffer.find(incoming_packet->seq_no) == buffer.end() &&
+                    incoming_packet->seq_no > nextPacketId) {
+            // save this packet into the map
+            buffer.insert({incoming_packet->seq_no, incoming_packet});
+        } else {
+            free(incoming_packet);
+            incoming_packet = NULL;
         }
 
         // send ack
